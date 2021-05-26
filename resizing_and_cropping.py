aaceffe53c87a77fde475e9a35de93757b2a534f
Clotilde LEVESQUE <clotilde.levesque@epita.fr>
2021-05-26 10:35:16 +0200

    feat: resizing & cropping

---
+++ resizing_and_cropping.py
@@ -0,0 +1,14 @@
+import cv2
+
+img = cv2.imread("resources/lena.png")
+
+# resize
+imgResize = cv2.resize(img, (1024, 1024))
+
+# crop
+imgCropped = img[0:200, 300:512]
+
+cv2.imshow("Output", img)
+cv2.imshow("resized", imgResize)
+cv2.imshow("cropped", imgCropped)
+cv2.waitKey(5000)
