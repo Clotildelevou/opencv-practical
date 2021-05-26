9e48e4cfc0812de79c30c80c24d78930769f1274
Clotilde LEVESQUE <clotilde.levesque@epita.fr>
2021-05-26 09:35:14 +0200

    feat: read with opencv

---
+++ read.py
@@ -0,0 +1,33 @@
+import cv2
+
+#image
+img = cv2.imread("ressources/lena.png")
+cv2.imshow("Output", img)
+cv2.waitKey(5000)
+
+#video
+cap = cv2.VideoCapture("ressources/test_vid.mp4")
+while True:
+    success, img = cap.read()
+    if success:
+        cv2.imshow("Video", img)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    else:
+        break
+
+#webcam
+cap = cv2.VideoCapture(0)
+cap.set(3, 640) #width
+cap.set(1, 480) #height
+
+while True:
+    success, img = cap.read()
+    if success:
+        cv2.imshow("Video", img)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    else:
+        break
