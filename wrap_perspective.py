a8904f94ade3ed5053df0eeee6c62c2cee7c96a1
Clotilde LEVESQUE <clotilde.levesque@epita.fr>
2021-05-26 11:59:48 +0200

    feat: wrap perspectives

---
+++ wrap_perspective.py
@@ -0,0 +1,15 @@
+import cv2
+import numpy as np
+
+#image
+img = cv2.imread("resources/cards.jpg")
+width, height = img.shape[1], img.shape[0]
+cards_points = np.float32([[286, 55], [394, 54], [289, 185], [417, 184]])
+img_points = np.float32([[0, 0], [width, 0], [0, height], [width, height]])
+
+matrix = cv2.getPerspectiveTransform(cards_points, img_points)
+imgOut = cv2.warpPerspective(img, matrix, (width, height))
+
+cv2.imshow("Output2", img)
+cv2.imshow("Output", imgOut)
+cv2.waitKey(0)
